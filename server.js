4892b170c829fc54ec3e2b6c8b425c6f982a205b
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-06-03 18:43:12 -0400

    added node mod

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 4000
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "index.html");
+    res.sendFile(__dirname + "/index.html");
   });
 
 
