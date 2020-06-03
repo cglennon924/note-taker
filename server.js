673b4f0b96791c4903a61769b02a92c037ad89e6
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-06-03 18:32:45 -0400

    added listener

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const fs = require("fs")
 const app= express()
 
 const PORT = process.env.PORT || 4000
+
+// Listener
+// ===========================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
